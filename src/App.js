1a15353eb66b5261905a85702c8e0e17b6adb47a
aditya <adityakunghadkar94.com>
2023-12-07 11:03:15 +0530

    File Updated

--- src/App.js
+++ src/App.js
@@ -27,17 +27,23 @@ function Logo() {
 
 function Form() {
   const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState(5);
+  const [quantity, setQuantity] = useState(1);
 
   function handleSubmit(e) {
     e.preventDefault();
     console.log(e);
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you do for your Trip 😍</h3>
-      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
