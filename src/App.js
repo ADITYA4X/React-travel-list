552d99e005d2de39795b1ef311ba39d764936c7e
aditya <adityakunghadkar94.com>
2023-12-07 10:29:46 +0530

    File Updated

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -24,6 +26,8 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+
   function handleSubmit(e) {
     e.preventDefault();
     console.log(e);
@@ -31,7 +35,7 @@ function Form() {
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
-      <h3>What do you for your Trip 😍</h3>
+      <h3>What do you do for your Trip 😍</h3>
       <select>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
@@ -39,7 +43,12 @@ function Form() {
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
